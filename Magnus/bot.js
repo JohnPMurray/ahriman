763210d5c1cd9697d0fad6557c11f75622551a90
John Murray <john.p.murray@tsgforce.com>
2023-07-14 16:20:02 -0400

    added datasheets

--- Magnus/bot.js
+++ Magnus/bot.js
@@ -1,77 +1,60 @@
-// const eris = require('eris');
-// const fs = require('fs')
-//
-// console.log('testing');
-// // Create a Client instance with our bot token.
-// const bot = new eris.Client('MTExNzUwODMyNzg5OTkzODg0Ng.GMoRy0.nS5Mvk2WenaSDQ0fEHRjTmSb1zMLhHbuBP8Qqk');
-//
-// // When the bot is connected and ready, log to console.
-// bot.on('ready', () => {
-//    console.log('Connected and ready.');
-// });
-//
-// // Every time a message is sent anywhere the bot is present,
-// // this event will fire and we will check if the bot was mentioned.
-// // If it was, the bot will attempt to respond with "Present".
-// bot.on('messageCreate', async (msg) => {
-//    const botWasMentioned = msg.mentions.find(
-//        mentionedUser => mentionedUser.id === bot.user.id,
-//    );
-//
-//    if (botWasMentioned) {
-//        try {
-//          //await msg.channel.createMessage('Present');
-//            fs.readFile( 'broodlord-1.png', (err, f)=> {
-//              console.log(f)
-//             msg.channel.createMessage({file: {name: 'broodlord.pdf', file: f}})
-//           })
-//        } catch (err) {
-//            // There are various reasons why sending a message may fail.
-//            // The API might time out or choke and return a 5xx status,
-//            // or the bot may not have permission to send the
-//            // message (403 status).
-//            console.warn('Failed to respond to mention.');
-//            console.warn(err);
-//        }
-//    }
-// });
-//
-// bot.on('error', err => {
-//    console.warn(err);
-// });
-//
-// bot.connect();
 
 const Discord = require("discord.js")
+const difflib = require("difflib")
 const fs = require("fs")
 const client = new Discord.Client()
+
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-const files = []
+client.commands = new Discord.Collection();
+const files = {}
+
 fs.readdir('datasheets', (err, directory) => {
   directory.forEach(file => {
-    console.log(file)
-    files.push(file)
+    name=file.match(/([^0-9]*)(\([0-9]\))?\.png/)
+    console.log(name[1])
+    if (files[name[1]]){
+      console.log('second entry for ' + name[1])
+      files[name[1]].push(file)
+    }
+    else{
+      console.log('first entry for ' + name[1])
+      files[name[1]] = [file]
+    }
   });
 });
 console.log(files)
 
 client.on("message", msg => {
   if (msg.content.match(/\{\{.*\}\}/)) {
-    let r = msg.content.match(/\{\{(.*)\}\}/)
+    let r = msg.content.match(/\{\{(.*)\}\}/
+  x
+  )
     if (r[1] == 'you'){
       msg.channel.send({ files: [{ attachment: 'ahriman.png' }] });
-    }else if (r[1].length >= 3 && r[1] != 'png' && r[1] != '.png'){
-      files.forEach((item, i) => {
-        if(item.includes(r[1])){
-          msg.channel.send({ files: [{ attachment: 'datasheets/'+item }] });
-        }
-      });
+    }else{
+      console.log(Object.keys(files))
+      console.log(r[1])
+      upper=r[1].toUpperCase()
+
+      matches = difflib.getCloseMatches(upper, Object.keys(files))
+      console.log(matches)
+      if (matches.length > 0){
+        files[matches[0]].forEach((item, i) => {
+          msg.channel.send({ files: [{ attachment: 'datasheets/'+item }] })
+        })
+
+      }else{
+        msg.channel.send('answer unclear, ask again later');
+      }
+
     }
   }
 })
 
-client.login('MTExNzUwODMyNzg5OTkzODg0Ng.GMoRy0.nS5Mvk2WenaSDQ0fEHRjTmSb1zMLhHbuBP8Qqk')
+
+
+client.login('')
